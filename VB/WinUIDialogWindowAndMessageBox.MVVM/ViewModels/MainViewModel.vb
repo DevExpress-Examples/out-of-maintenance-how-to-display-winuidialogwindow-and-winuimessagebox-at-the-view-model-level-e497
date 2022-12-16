7168f5b062221904691851d67b7f82feab12e2e7
DevExpressExampleBot <fakeEmail>
2022-12-17 02:34:42 +0400

    Source auto update [skip ci]

--- VB/WinUIDialogWindowAndMessageBox.MVVM/ViewModels/MainViewModel.vb
+++ VB/WinUIDialogWindowAndMessageBox.MVVM/ViewModels/MainViewModel.vb
@@ -1,48 +1,55 @@
-﻿Imports System
 Imports DevExpress.Mvvm
 Imports DevExpress.Mvvm.Xpf
 Imports System.Windows.Input
 Imports System.Windows
 
 Namespace WinUIDialogWindowAndMessageBox.MVVM.ViewModels
+
     Public Class MainViewModel
         Inherits ViewModelBase
 
-        Private privateShowDialogWindowCommand As ICommand
-        Public Property ShowDialogWindowCommand() As ICommand
+        Private _ShowDialogWindowCommand As ICommand, _ShowMessageBoxCommand As ICommand
+
+        Public Property ShowDialogWindowCommand As ICommand
             Get
-                Return privateShowDialogWindowCommand
+                Return _ShowDialogWindowCommand
             End Get
+
             Private Set(ByVal value As ICommand)
-                privateShowDialogWindowCommand = value
+                _ShowDialogWindowCommand = value
             End Set
         End Property
-        Private privateShowMessageBoxCommand As ICommand
-        Public Property ShowMessageBoxCommand() As ICommand
+
+        Public Property ShowMessageBoxCommand As ICommand
             Get
-                Return privateShowMessageBoxCommand
+                Return _ShowMessageBoxCommand
             End Get
+
             Private Set(ByVal value As ICommand)
-                privateShowMessageBoxCommand = value
+                _ShowMessageBoxCommand = value
             End Set
         End Property
+
         Public Sub New()
             ShowDialogWindowCommand = New DelegateCommand(AddressOf ShowDialogWindow)
             ShowMessageBoxCommand = New DelegateCommand(AddressOf ShowMessageBox)
         End Sub
+
         Private Sub ShowDialogWindow()
             DialogService.ShowDialog(MessageBoxButton.OKCancel, "Test", "TestView", Nothing, Me)
         End Sub
+
         Private Sub ShowMessageBox()
             MessageBoxService.Show("MessageBox", "Test", MessageBoxButton.OKCancel, MessageBoxImage.Information)
         End Sub
 
-        Private ReadOnly Property DialogService() As IDialogService
+        Private ReadOnly Property DialogService As IDialogService
             Get
                 Return GetService(Of IDialogService)()
             End Get
         End Property
-        Private ReadOnly Property MessageBoxService() As IMessageBoxService
+
+        Private ReadOnly Property MessageBoxService As IMessageBoxService
             Get
                 Return GetService(Of IMessageBoxService)()
             End Get
--- VB/WinUIDialogWindowAndMessageBox/MainWindow.xaml.vb
+++ VB/WinUIDialogWindowAndMessageBox/MainWindow.xaml.vb
@@ -1,33 +1,22 @@
-﻿Imports DevExpress.Xpf.Core
+Imports DevExpress.Xpf.Core
 Imports DevExpress.Xpf.WindowsUI
-Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
 Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 
 Namespace WinUIDialogWindowAndMessageBox
+
     ''' <summary>
     ''' Interaction logic for MainWindow.xaml
     ''' </summary>
-    Partial Public Class MainWindow
+    Public Partial Class MainWindow
         Inherits DXWindow
 
         Public Sub New()
-            InitializeComponent()
+            Me.InitializeComponent()
         End Sub
 
         Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
-            Dim dialog As New WinUIDialogWindow("Test", MessageBoxButton.OKCancel)
+            Dim dialog As WinUIDialogWindow = New WinUIDialogWindow("Test", MessageBoxButton.OKCancel)
             dialog.Content = New TestUserControl()
             dialog.ShowDialogWindow()
         End Sub
