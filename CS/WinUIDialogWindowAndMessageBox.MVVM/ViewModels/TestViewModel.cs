78b5b428fc2bf8b0b591f684fc93fe933612fcff
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-10-16 17:47:39 +0400

    Change example status to out of maintenance

--- CS/WinUIDialogWindowAndMessageBox.MVVM/ViewModels/TestViewModel.cs
+++
@@ -1,8 +0,0 @@
-using System;
-using DevExpress.Mvvm;
-
-namespace WinUIDialogWindowAndMessageBox.MVVM.ViewModels {
-    public class TestViewModel : ViewModelBase {
-        public string Name { get; set; }
-    }
-}
